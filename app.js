6ab8a45a0840d38c70f756376aa6bde7b7cd5a35
John Doe <johndoe@email.com>
2024-06-04 04:24:07 +0700

    5

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ document.getElementById('menu-icon').addEventListener('click', function() {
     navLinks.classList.toggle('show');
     this.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('toggleButton');
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            toggleButton.textContent = 'Switch to Light Mode';
+        } else {
+            toggleButton.textContent = 'Switch to Dark Mode';
+        }
+    });
+});
